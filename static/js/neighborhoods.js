4869ca9a158954389cf66fbcc3b9f639788957e5
TrevorLovesMommy <melissa1lin@gmail.com>
2020-03-26 07:40:11 -0700

    updating app.py and js scripts

--- static/js/neighborhoods.js
+++ static/js/neighborhoods.js
@@ -1,1 +1,15 @@
+// api url found http://127.0.0.1:5000/api/v1.0/neighborhoods";
+// d3 tag is id="neighborhoods" for the chart
+// d3 tag is id="selNeighborhood" for the pulldown
+
 console.log("neighborhood.js successfully called from static/js!")
+
+d3.select("#neighborhoods").text("Hey, I changed this text with d3 from neighborhoods.js");
+
+d3.json("/api/v1.0/neighborhoods").then(function(data) {
+  console.log(data);
+})
+.catch(function(error) {
+  console.log("error in d3.json");
+});
+
---
+++ static/mapping.js
@@ -0,0 +1,18 @@
+// api url found http://127.0.0.1:5000/api/v1.0/mapping";
+// d3 tag is id = #mapping
+
+//Ilya - you also have a config.js in this same folder for you API key
+
+
+
+
+console.log("mapping.js successfully called from static/js!")
+
+d3.select("#mapping").text("Hey, I changed this text with d3 from mapping.js");
+
+d3.json("/api/v1.0/mapping").then(function(data) {
+  console.log(data);
+})
+.catch(function(error) {
+  console.log("error in d3.json");
+});
