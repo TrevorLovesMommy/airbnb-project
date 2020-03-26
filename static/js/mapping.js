43eabb5588cf680ab4e43776614db2d38830d864
TrevorLovesMommy <melissa1lin@gmail.com>
2020-03-26 12:32:00 -0700

    updating app.py and js scripts

--- static/js/mapping.js
+++ static/js/mapping.js
@@ -1,1 +1,15 @@
-console.log("mapping.js successfully called from static/js folder!")
+// api url found http://127.0.0.1:5000/api/v1.0/mapping";
+// d3 tag is id = #mapping
+
+
+console.log("roomtype.js successfully called from static/js!")
+
+d3.select("#mapping").text("Hey, I changed this text with d3 from mapping.js");
+
+d3.json("/api/v1.0/mapping").then(function(data) {
+  console.log(data);
+})
+.catch(function(error) {
+  console.log("error in d3.json");
+});
+
