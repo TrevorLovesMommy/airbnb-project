72b059853595ddc54234a7a05791f92832cf4bfb
TrevorLovesMommy <melissa1lin@gmail.com>
2020-03-30 13:51:38 -0700

    updating roomtype.js with new chart

--- static/js/roomtype.js
+++ static/js/roomtype.js
@@ -1,5 +1,16 @@
 // app route for jsonified data at http://127.0.0.1:5000/api/v1.0/roomtypes"
 
+
+//global variables
+//data for sfHousingStock from https://default.sfplanning.org/publications_reports/2018_Housing_Inventory.pdf
+var sfHousingStock = 394615;
+//two thirds of housing stock is used for long term rentals.  https://housing.datasf.org/overview/
+var ltRentalStock = Math.round((sfHousingStock*2)/3);
+
+console.log(sfHousingStock)
+console.log(ltRentalStock)
+
+//read jasonofied data from app route http://127.0.0.1:5000/api/v1.0/roomtypes
 d3.json("/api/v1.0/roomtypes").then(function(data) {
   console.log("here is the data");
   console.log(data);
@@ -16,9 +27,12 @@ d3.json("/api/v1.0/roomtypes").then(function(data) {
   var sharedRoomCount = unitCount[2];
   console.log(`this is the shared room count ${sharedRoomCount}`) 
 
+
+  //Ask Justin how to make totalCount global so I can use it in legal.js
   var totalCount = entireHouseCount + privateRoomCount + sharedRoomCount;
   var chartTitle = (`Room Types on SF Airbnb (Total ${totalCount})`);
- 
+
+  //create room type chart
   //   * ---------------------------------------
   //   * This chart was created using Anychart.
   //   * 
@@ -29,7 +43,7 @@ d3.json("/api/v1.0/roomtypes").then(function(data) {
   anychart.onDocumentReady(function () {
   // data set
   var data = [
-    {x: "Entire House", value: entireHouseCount},
+    {x: "Entire House/Apt", value: entireHouseCount},
     {x: "Private Room", value: privateRoomCount},
     {x: "Shared Room", value: sharedRoomCount},
   ];
@@ -49,6 +63,66 @@ d3.json("/api/v1.0/roomtypes").then(function(data) {
   chart1.draw();
 
   });
+
+  //create chart comparing Airbnb Entire Home Listing to SF Long-term housing rental stock
+  //   * ---------------------------------------
+  //   * This chart was created using Anychart.
+  //   * https://www.anychart.com/products/anychart/gallery/Column_Charts/Column_Chart.php
+  //   * ---------------------------------------
+
+  anychart.onDocumentReady(function () {
+    // create column chart
+    var chart = anychart.column();
+
+    // turn on chart animation
+    chart.animation(true);
+
+    // set chart title text settings
+    chart.title('Airbnb Entire Home/Apt compared to Long Term Rental Stock');
+
+    // create area series with passed data
+    var series = chart.column([
+        // ['All SF Dwelling Units', sfHousingStock],
+        ['Long-term Rental Stock', ltRentalStock],
+        ['Airbnb Entire Home/Apt', entireHouseCount],
+    ]);
+
+    // set series tooltip settings
+    // series.tooltip().titleFormat('{%X}');
+
+    // series.tooltip()
+    //         .position('center-top')
+    //         .anchor('center-bottom')
+    //         .offsetX(0)
+    //         .offsetY(5)
+    //         .format('${%Value}{groupsSeparator: }');
+    series.tooltip().enabled(true).title().enabled(true).text("Information:");
+    series.labels().enabled(true).anchor("top-center").position("top-center").fontSize(13);
+
+
+    // set scale minimum
+    chart.yScale().minimum(0);
+
+    // set yAxis labels formatter
+    chart.yAxis().labels().format('${%Value}{groupsSeparator: }');
+
+    // tooltips position and interactivity settings
+    chart.tooltip().positionMode('point');
+    chart.interactivity().hoverMode('by-x');
+
+    // axes titles
+    chart.xAxis().title('Housing categories');
+    chart.yAxis().title('Number of units');
+
+    // set container id for the chart
+    chart.container('longtermrentals');
+
+    // initiate chart drawing
+    chart.draw();
+});
+
+
+
 });
 
 
